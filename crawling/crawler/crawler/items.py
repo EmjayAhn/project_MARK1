758243d24707e93d5c546edd14aae95a6429693b
EmjayAhn <emjay.data@gmail.com>
2019-01-29 15:58:29 +0900

    Feat: modify crawler

--- crawling/crawler/crawler/items.py
+++ crawling/crawler/crawler/items.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    # data : 출원일
+    # mark_status : 출원, 거절, 등록 등의 현재 상표 상태
+    # mark_class : 상품 분류
+    date = scrapy.Field()
+    mark_status = scrapy.Field()
+    mark_class = scrapy.Field()
